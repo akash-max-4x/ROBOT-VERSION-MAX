4b649641f4aad62bd4f70b5d802139a774c3ffe2
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-09-16 21:21:55 +0600

    Update sing.js

--- scripts/commands/sing.js
+++ scripts/commands/sing.js
@@ -102,7 +102,7 @@ async function video(api, event, args, message) {
 
 module.exports = {
     config: {
-        name: "sing", 
+        name: "song", 
         version: "1.0",
         author: "Vex_Kshitiz",
         countDown: 10,
