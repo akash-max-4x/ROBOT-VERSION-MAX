9a8d3903caf24bf9ec1960dc97c95956ab9a5321
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-09-20 15:06:41 +0600

    Update baby.js

--- scripts/commands/baby.js
+++ scripts/commands/baby.js
@@ -21,7 +21,7 @@ module.exports = {
       console.log(response.data);
       const result = response.data.data.msg;
 
-
+      
       api.sendMessage(result, event.threadID, (error, info) => {
         if (error) {
           console.error('Error replying to user:', error);
@@ -43,9 +43,52 @@ module.exports = {
   },
 
 
-
+  
   start: async function ({ nayan, events, args, Users }) {
     try {
       const msg = args.join(" ");
       if (!msg) {
-        var tl = ["hea baby_//-🫦😻","bØlo gøø jan amr _//-😘","
+        var tl = ["Hum Baby Bolo🐱"]
+        var name = await Users.getNameUser(events.senderID);
+        var rand = tl[Math.floor(Math.random() * tl.length)]
+        return nayan.sendMessage({ 
+              body: `${name}, ${rand}`, 
+              mentions: [{ tag: name, id: events.senderID }] }, events.threadID, (error, info) => {
+          if (error) {
+            return nayan.sendMessage('An error occurred while processing your request. Please try again later.', events.threadID, events.messageID);
+          }
+
+          global.client.handleReply.push({
+            type: 'reply',
+            name: this.config.name,
+            messageID: info.messageID,
+            author: events.senderID,
+            head: msg,
+          });
+        }, events.messageID);
+    }
+
+      const response = await axios.get(`http://37.27.114.136:25472/sim?type=ask&ask=${encodeURIComponent(msg)}`);
+      console.log(response.data);
+      const replyMessage = response.data.data.msg;
+
+      nayan.sendMessage({ body: replyMessage }, events.threadID, (error, info) => {
+        if (error) {
+          return nayan.sendMessage('An error occurred while processing your request. Please try again later.', events.threadID, events.messageID);
+        }
+
+        global.client.handleReply.push({
+          type: 'reply',
+          name: this.config.name,
+          messageID: info.messageID,
+          author: events.senderID,
+          head: msg,
+        });
+      }, events.messageID);
+
+    } catch (error) {
+      console.log(error)
+      nayan.sendMessage('An error has occurred, please try again later.', events.threadID, events.messageID);
+    }
+  }
+};
